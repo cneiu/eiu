edd33f0a1cacd6700d60d00760dbd607658bcf21
Bobby <ziee@qq.com>
2017-08-13 19:29:06 +0800

    add captcha component

---
+++ src/components/captcha/ICaptchaBuilderInterface.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * EIU PHP FRAMEWORK
+ *
+ * @author        成都东联智胜软件有限公司
+ * @link          https://www.cneiu.com
+ */
+
+
+namespace eiu\components\captcha;
+
+/**
+ * 验证码接口
+ * Interface CaptchaBuilderInterface
+ * @package Minho\Captcha
+ */
+interface ICaptchaBuilderInterface
+{
+    /**
+     * 创建验证图片
+     * @return mixed
+     */
+    public function create();
+
+    /**
+     * 将验证码图片保存到指定路径
+     * @param string $filename 物理路径
+     * @param int $quality 清晰度
+     * @return mixed
+     */
+    public function save($filename, $quality);
+
+    /**
+     * 获取验证码图片
+     * @param int $quality 清晰度
+     * @return mixed
+     */
+    public function output($quality);
+
+    /**
+     * 获取验证码内容
+     * @return mixed
+     */
+    public function getText();
+}
